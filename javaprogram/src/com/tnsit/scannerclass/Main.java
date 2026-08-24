4f6830c1d9b08d48615b9f31e1d09d322975f174
tnsda <your-github-email@example.com>
2026-08-24 13:58:16 +0530

    commit

--- javaprogram/src/com/tnsit/scannerclass/Main.java
+++ javaprogram/src/com/tnsit/scannerclass/Main.java
@@ -1,5 +1,23 @@
 package com.tnsit.scannerclass;
 
+import java.util.Scanner;
+
 public class Main {
 
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter your name:");
+		String name=sc.nextLine();
+		
+		System.out.println("Enter your age:");
+	    int age=sc.nextInt();
+	    
+	    System.out.println("Enter your salary:");
+	    double salary=sc.nextDouble();
+	    
+	    System.out.println("Name:"+name);
+	    System.out.println("Age:"+age);
+	    System.out.println("Salary:"+salary);
+	}
 }
--- javaprogram/src/com/tnsit/scannerclass/scannerclass.java
+++ javaprogram/src/com/tnsit/scannerclass/scannerclass.java
@@ -1,7 +1,20 @@
 package com.tnsit.scannerclass;
 
-public class scannerclass {
+import java.util.Scanner;
 
+public class scannerclass {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	
+	System.out.println("Enter the first number:");
+	int number1=sc.nextInt();
+	
+	System.out.println("Enter the second number:");
+	int number2=sc.nextInt();
+	
+	int sum =number1+number2;
+	System.out.println(" The sum of numbers " + number1 +" and "+ number2 +" is: "+sum);
+	sc.close();
+}
 }
 
-+
